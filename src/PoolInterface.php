d54c2b0ad3e810995f7b3300ebc10cbe7bafc744
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-05-11 17:44:52 +0200

    Return pool information through the info method

--- src/PoolInterface.php
+++ src/PoolInterface.php
@@ -2,12 +2,13 @@
 
 namespace WyriHaximus\React\ChildProcess\Pool;
 
+use Evenement\EventEmitterInterface;
 use React\ChildProcess\Process;
 use React\EventLoop\LoopInterface;
 use React\Promise\PromiseInterface;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Call;
 
-interface PoolInterface
+interface PoolInterface extends EventEmitterInterface
 {
     /**
      * @param Process $process
@@ -16,10 +17,14 @@ interface PoolInterface
      */
     public function __construct(Process $process, LoopInterface $loop, array $options = []);
 
-
     /**
      * @param Call $message
      * @return PromiseInterface
      */
     public function rpc(Call $message);
+
+    /**
+     * @return array
+     */
+    public function info();
 }
