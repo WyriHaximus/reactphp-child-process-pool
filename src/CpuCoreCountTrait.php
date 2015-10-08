393e9a14328adcf8b91b59c8aacc90ed5e69b626
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-10-08 22:30:20 +0200

    Keep track of which cores are available to assign a process to

--- src/CpuCoreCountTrait.php
+++ src/CpuCoreCountTrait.php
@@ -14,6 +14,9 @@ use WyriHaximus\React\ChildProcess\Pool\Strategy\Core\CountInterface;
 
 trait CpuCoreCountTrait
 {
+    protected $coreMessengerMapping = [];
+    protected $availableAddresses = [];
+
     protected function setUpStrategies()
     {
         if (
@@ -40,24 +43,65 @@ trait CpuCoreCountTrait
 
     protected function resolveCoreAddresses($coreCount)
     {
-        $addresses = [];
         for ($i = 0; $i < $coreCount; $i++) {
-            $addresses[] = $i;
+            $this->availableAddresses[$i] = $i;
         }
 
-        return \React\Promise\resolve($addresses);
+        return \React\Promise\resolve($this->availableAddresses);
     }
 
     protected function spawnProcessAtAddress($address)
     {
+        $this->startingProcesses++;
+        unset($this->availableAddresses[$address]);
         $processOptions = isset($this->options['processOptions']) ? $this->options['processOptions'] : [];
         $process = $this->rebuildProcess($address);
-        Factory::parent($process, $this->loop, $processOptions)->then(function (Messenger $messenger) {
+        Factory::parent($process, $this->loop, $processOptions)->then(function (Messenger $messenger) use ($address) {
+            $this->startingProcesses--;
             $this->pool->attach($messenger);
             $this->readyPool->enqueue($messenger);
+            $this->coreMessengerMapping[spl_object_hash($messenger)] = $address;
+            $messenger->on('exit', function () use ($messenger, $address) {
+                $this->pool->detach($messenger);
+                $this->availableAddresses[$address] = $address;
+                unset($this->coreMessengerMapping[spl_object_hash($messenger)]);
+                $messengers = [];
+                while ($this->readyPool->count() > 0) {
+                    $queuedMessenger = $this->readyPool->dequeue();
+                    if ($queuedMessenger === $messenger) {
+                        continue;
+                    }
+                    $messengers[] = $queuedMessenger;
+                }
+
+                array_walk($messengers, function (Messenger $messenger) {
+                    $this->readyPool->enqueue($messenger);
+                });
+
+                if (
+                    $this->callQueue->count() > 0 &&
+                    $this->readyPool->count() <= $this->options['min_size'] &&
+                    (
+                        $this->startingProcesses + $this->pool->count()
+                    ) < $this->options['max_size']
+                ) {
+                    $this->spawnProcess();
+                }
+            });
         }, function ($error) {
+            $this->startingProcesses--;
             $this->emit('error', [$error, $this]);
         });
+    }
+
+    protected function getFreeAddress()
+    {
+        foreach ($this->availableAddresses as $address) {
+            unset($this->availableAddresses[$address]);
+            return $address;
+        }
+
+        throw new \Exception('All cores in use!');
     }
 
     protected function rebuildProcess($address)
