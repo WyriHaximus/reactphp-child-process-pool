adaee44f9e7fcf7d74942bc9056fcf1fad3ec36e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-19 21:42:12 +0100

    Updated first example to new API

--- examples/return-class/ping.php
+++ examples/return-class/ping.php
@@ -2,33 +2,28 @@
 
 require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
-use React\ChildProcess\Process;
 use React\EventLoop\Factory as EventLoopFactory;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory as MessagesFactory;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
-use WyriHaximus\React\ChildProcess\Pool\CpuCoreCountFixedPool;
+use WyriHaximus\React\ChildProcess\Pool\Pool\Fixed;
 
 $loop = EventLoopFactory::create();
 
-$poolClass = CpuCoreCountFixedPool::class;
-
-$messenger = new $poolClass(new Process(''), $loop, [
-    'processClassName' => 'WyriHaximus\React\ChildProcess\Messenger\ReturnChild',
-]);
+$pool = Fixed::createFromClass('WyriHaximus\React\ChildProcess\Messenger\ReturnChild', $loop);
 
 $i = 0;
 for ($i = 0; $i < 100; $i++) {
     echo $i, PHP_EOL;
 
-    $messenger->rpc(MessagesFactory::rpc('return', [
+    $pool->rpc(MessagesFactory::rpc('return', [
         'i' => $i,
         'time' => time(),
         'string' => str_pad('0', 1024 * 1024 * 5)
-    ]))->then(function (Payload $payload) use ($messenger) {
+    ]))->then(function (Payload $payload) use ($pool) {
         echo $payload['i'], PHP_EOL;
         echo $payload['time'], PHP_EOL;
         if ($payload['i'] == 99) {
-            $messenger->terminate();
+            $pool->terminate();
         }
     });
 }
