b236cd04dcfd30c417572ac3ad7ca3e7eddb8900
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-20 22:20:06 +0100

    Worker interface

---
+++ src/WorkerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WyriHaximus\React\ChildProcess\Pool;
+
+use Evenement\EventEmitterInterface;
+use React\Promise\PromiseInterface;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Rpc;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+
+interface WorkerInterface extends EventEmitterInterface
+{
+    /**
+     * @param Messenger $messenger
+     */
+    public function __construct(Messenger $messenger);
+
+    /**
+     * @param Rpc $rpc
+     * @return PromiseInterface
+     */
+    public function rpc(Rpc $rpc);
+
+    /**
+     * @return bool
+     */
+    public function isBusy();
+}
