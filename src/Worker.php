a5f06d36f6d5d4562c00f4e52f4d1188fd4e805f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-22 07:47:31 +0100

    Worker to do the actual messenger interaction

---
+++ src/Worker.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace WyriHaximus\React\ChildProcess\Pool;
+
+use Evenement\EventEmitterTrait;
+use React\Promise\PromiseInterface;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Rpc;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+
+class Worker implements WorkerInterface
+{
+    use EventEmitterTrait;
+
+    /**
+     * @var Messenger
+     */
+    protected $messenger;
+
+    /**
+     * @var bool
+     */
+    protected $busy = false;
+
+    /**
+     * @param Messenger $messenger
+     */
+    public function __construct(Messenger $messenger)
+    {
+        $this->messenger = $messenger;
+    }
+
+    /**
+     * @param Rpc $rpc
+     * @return PromiseInterface
+     */
+    public function rpc(Rpc $rpc)
+    {
+        $this->busy = true;
+        return $this->messenger->rpc($rpc)->always(function () {
+            $this->busy = false;
+            $this->emit('done', [$this]);
+        });
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBusy()
+    {
+        return $this->busy;
+    }
+}
