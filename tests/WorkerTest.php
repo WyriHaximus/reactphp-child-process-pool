365da10616242280d6179b55a317e2281faac2a6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-01 22:13:29 +0100

    Tests for the worker

---
+++ tests/WorkerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WyriHaximus\React\Tests\ChildProcess\Pool;
+
+use Phake;
+use React\Promise\Deferred;
+use React\Promise\FulfilledPromise;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory;
+use WyriHaximus\React\ChildProcess\Pool\Worker;
+
+class WorkerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWorker()
+    {
+        $messenger = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        $worker = new Worker($messenger);
+
+        $this->assertFalse($worker->isBusy());
+
+        $calledCallback = false;
+        $worker->on('terminating', function (Worker $passedWorker) use (&$calledCallback, $worker) {
+            $this->assertSame($worker, $passedWorker);
+            $calledCallback = true;
+        });
+        $worker->terminate();
+        $this->assertTrue($worker->isBusy());
+        $this->assertTrue($calledCallback);
+        Phake::verify($messenger)->softTerminate();
+    }
+
+    public function testRpc()
+    {
+        $deferred = new Deferred();
+        $rpc = Factory::rpc('t', []);
+        $messenger = Phake::mock('WyriHaximus\React\ChildProcess\Messenger\Messenger');
+        Phake::when($messenger)->rpc($rpc)->thenReturn($deferred->promise());
+        $worker = new Worker($messenger);
+
+        $this->assertFalse($worker->isBusy());
+        $worker->rpc($rpc);
+        $this->assertTrue($worker->isBusy());
+        $deferred->resolve();
+        $this->assertFalse($worker->isBusy());
+        Phake::verify($messenger)->rpc($rpc);
+    }
+}
