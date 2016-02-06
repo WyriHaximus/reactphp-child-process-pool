7a01293089254bf096ebc3c78dcc43a591e1dcad
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-06 16:19:43 +0100

    Basic create fixed pool tests

---
+++ tests/Pool/FixedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WyriHaximus\React\Tests\ChildProcess\Pool\Pool;
+
+use Phake;
+use WyriHaximus\React\ChildProcess\Pool\Pool\Fixed;
+
+class FixedTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $process = Phake::mock('React\ChildProcess\Process');
+        $loop = Phake::mock('React\EventLoop\LoopInterface');
+        $poolPromise = Fixed::create($process, $loop);
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $poolPromise);
+        $promiseHasResolved = false;
+        $poolPromise->then(function ($pool) use (&$promiseHasResolved) {
+            $this->assertInstanceOf('WyriHaximus\React\ChildProcess\Pool\Pool\Fixed', $pool);
+            $promiseHasResolved = true;
+        });
+        $this->assertTrue($promiseHasResolved);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Given class doesn't implement ChildInterface
+     */
+    public function testCreateFromClass()
+    {
+        $loop = Phake::mock('React\EventLoop\LoopInterface');
+        Fixed::createFromClass('stdClass', $loop);
+    }
+}
