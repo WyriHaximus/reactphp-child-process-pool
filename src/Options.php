b7c63c5e66bb484406de2075f23c7794bfb388a2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-06 23:16:02 +0100

    Added size related options

--- src/Options.php
+++ src/Options.php
@@ -5,7 +5,10 @@ namespace WyriHaximus\React\ChildProcess\Pool;
 // @codeCoverageIgnoreStart
 class Options
 {
-    const MANAGER = 'manager';
-    const QUEUE   = 'queue';
+    const MANAGER  = 'manager';
+    const QUEUE    = 'queue';
+    const SIZE     = 'size';
+    const MIN_SIZE = 'min_size';
+    const MAX_SIZE = 'max_size';
 }
 // @codeCoverageIgnoreEnd
