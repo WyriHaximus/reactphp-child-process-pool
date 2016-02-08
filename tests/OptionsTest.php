0f672e33157f97deefccd48058227214ba90165b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-08 23:09:30 +0100

    Ensure all constants are strngs

--- tests/OptionsTest.php
+++ tests/OptionsTest.php
@@ -2,6 +2,8 @@
 
 namespace WyriHaximus\React\Tests\ChildProcess\Pool;
 
+use ReflectionClass;
+
 class OptionsTest extends \PHPUnit_Framework_TestCase
 {
     public function testNoMethods()
@@ -12,5 +14,12 @@ class OptionsTest extends \PHPUnit_Framework_TestCase
     public function testNoVars()
     {
         $this->assertSame(0, count(get_class_vars('WyriHaximus\React\ChildProcess\Pool\Options')));
+    }
+
+    public function testConstants()
+    {
+        foreach ((new ReflectionClass('WyriHaximus\React\ChildProcess\Pool\Options'))->getConstants() as $constant) {
+            $this->assertInternalType('string', $constant);
+        }
     }
 }
