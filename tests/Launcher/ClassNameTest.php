a8a3283a770dc84a54d557b131620cf19707d727
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-11 07:53:38 +0100

    Rewrote classname launcher test to NOT launch an actual process

--- tests/Launcher/ClassNameTest.php
+++ tests/Launcher/ClassNameTest.php
@@ -3,20 +3,20 @@
 namespace WyriHaximus\React\Tests\ChildProcess\Pool\Launcher;
 
 use Phake;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
 use WyriHaximus\React\ChildProcess\Pool\Launcher\ClassName;
 
 class ClassNameTest extends \PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Given class doesn't implement ChildInterface
+     */
     public function testClassName()
     {
         $loop = Phake::mock('React\EventLoop\LoopInterface');
-        $timerCalled = false;
-        Phake::when($loop)->addPeriodicTimer($this->isType('float'), $this->isType('callable'))->thenReturnCallback(function () use (&$timerCalled) {
-            $timerCalled = true;
-            return true;
-        });
-        $process = new ClassName('WyriHaximus\React\ChildProcess\Messenger\ReturnChild');
+        $process = new ClassName('stdClass');
         $process($loop, []);
-        $this->assertTrue($timerCalled);
     }
 }
