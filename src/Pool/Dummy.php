601f01a7d0bfc75dd14c2e8f4869af384d457eca
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-18 17:16:36 +0100

    Missing create method

--- src/Pool/Dummy.php
+++ src/Pool/Dummy.php
@@ -3,16 +3,24 @@
 namespace WyriHaximus\React\ChildProcess\Pool\Pool;
 
 use Evenement\EventEmitterTrait;
+use React\ChildProcess\Process as ChildProcess;
 use React\EventLoop\LoopInterface;
 use React\Promise\FulfilledPromise;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Message;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Rpc;
+use WyriHaximus\React\ChildProcess\Pool\Launcher\Process;
 use WyriHaximus\React\ChildProcess\Pool\PoolInterface;
+use WyriHaximus\React\ChildProcess\Pool\ProcessCollection\Single;
 use WyriHaximus\React\ChildProcess\Pool\ProcessCollectionInterface;
 
 class Dummy implements PoolInterface
 {
     use EventEmitterTrait;
+
+    public static function create(ChildProcess $process, LoopInterface $loop, array $options = [])
+    {
+        return new self(new Single(new Process($process)), $loop, $options);
+    }
 
     public function __construct(ProcessCollectionInterface $processCollection, LoopInterface $loop, array $options = [])
     {
