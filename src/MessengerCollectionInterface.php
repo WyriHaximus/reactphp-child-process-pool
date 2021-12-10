6e51d681f1830c017440aff7772958f5aebd4c4c
Grégory Planchat <gregory@kiboko.fr>
2021-12-10 08:02:50 +0100

    Added return types to iterator interfaces for PHP 8.1 compatibility

--- src/MessengerCollectionInterface.php
+++ src/MessengerCollectionInterface.php
@@ -6,22 +6,5 @@ use WyriHaximus\React\ChildProcess\Messenger\Messenger;
 
 interface MessengerCollectionInterface extends \Iterator
 {
-    /**
-     * @return Messenger
-     */
-    public function current();
-
-    /**
-     * @return mixed
-     */
-    public function key();
-
-    public function next();
-
-    public function rewind();
-
-    /**
-     * @return bool
-     */
-    public function valid();
+    public function current(): Messenger;
 }
--- src/ProcessCollectionInterface.php
+++ src/ProcessCollectionInterface.php
@@ -4,22 +4,5 @@ namespace WyriHaximus\React\ChildProcess\Pool;
 
 interface ProcessCollectionInterface extends \Iterator
 {
-    /**
-     * @return callable
-     */
-    public function current();
-
-    /**
-     * @return mixed
-     */
-    public function key();
-
-    public function next();
-
-    public function rewind();
-
-    /**
-     * @return bool
-     */
-    public function valid();
+    public function current(): callable;
 }
